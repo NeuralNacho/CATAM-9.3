a306366030687c8b7d483b065ed4af029d8f198d
NeuralNacho <anguswhittam@btinternet.com>
2022-09-06 00:20:49 +0100

    minor edit

--- edit_distance.py
+++ edit_distance.py
@@ -5,16 +5,17 @@ class edit_distance:
     def __init__(self, string1, string2):
         self.string1 = string1
         self.string2 = string2
+        self.distance_matrix = \
+            np.zeros( (len(self.string1) + 1, 
+                    len(self.string2) + 1) )
+
 
     def min_distance(self):
-        distance_matrix = np.zeros( (len(self.string1) + 1, 
-                                    len(self.string2) + 1) )
-        
         # Boundary conditions
         for i in range(len(self.string1) + 1):
-            distance_matrix[i][0] = i  
+            self.distance_matrix[i][0] = i  
         for j in range(len(self.string2) + 1):
-            distance_matrix[0][j] = j 
+            self.distance_matrix[0][j] = j 
 
         # Find D(i,j) for each i,j
         for i in range(len(self.string1)):
@@ -23,14 +24,15 @@ class edit_distance:
                 if self.string1[i] == self.string2[j]:
                     char_match = 0
 
-                distance_matrix[i + 1][j + 1] = \
-                min(distance_matrix[i + 1][j] + 1, 
-                    distance_matrix[i][j + 1] + 1, 
-                    distance_matrix[i][j] + char_match)
+                self.distance_matrix[i + 1][j + 1] = \
+                min(self.distance_matrix[i + 1][j] + 1, 
+                    self.distance_matrix[i][j + 1] + 1, 
+                    self.distance_matrix[i][j] + char_match)
 
-        return int(distance_matrix[len(self.string1)] \
+        return int(self.distance_matrix[len(self.string1)] \
                                 [len(self.string2)])                                    
 
 if __name__ == "__main__":
     test_case = edit_distance('shesells', 'seashells')
     print(test_case.min_distance())
+    print(test_case.distance_matrix)
